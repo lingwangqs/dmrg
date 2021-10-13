ea6090cb2bcaae5cc467a308f035b29bbb5ac536
Brian Gregor <bgregor@bu.edu>
2021-10-13 10:29:07 -0400

    Forgot to add this file in the previous check-in.

---
+++ typedefs.h
@@ -0,0 +1,13 @@
+#ifndef TYPEDEFS_
+#define TYPEDEFS_
+
+// Handy type definition declarations.  These are used to 
+// enable STL containers as replacements for C pointer arrays.
+
+#include <vector>
+using namespace std ;
+// 3D vector of doubles
+typedef vector<vector<vector<double>>> VecDbl3 ;
+ 
+
+#endif 
